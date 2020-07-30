150b34028764e7aa36fa0e87486b6d60dccc1506
Pratik Karki <karkipra@grinnell.edu>
2020-07-29 23:53:16 -0400

    implemented methods for the endpoints

---
+++ service/keywords.py
@@ -0,0 +1,20 @@
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+DEVELOPER_KEY = "AIzaSyC2gP7-BiIDFEEZ9nnRXdnKVAII5mmw2os"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def get_keywords(video_id):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+    # Call the videos.list method to retrieve results matching the specified query term.
+    search_response = youtube.videos().list(
+        part="statistics, snippet",
+        id=video_id
+    ).execute()
+
+    keywords = search_response['items'][0]['snippet']['tags']
+
+    return {"keywords": keywords}
