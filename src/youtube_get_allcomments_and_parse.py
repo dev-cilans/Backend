620ef4ec3239608424faa1c9119c21dab63a2e1e
edwhuang <edwhuang@umich.edu>
2020-08-17 22:32:32 -0400

    Add comment

--- src/youtube_get_allcomments_and_parse.py
+++ src/youtube_get_allcomments_and_parse.py
@@ -3,6 +3,7 @@ from v1.service.Comment.comments_helper import build_initial_request, parse_resu
 all_comments=list()
 
 def main():
+    # Build request and fetch comments
     request = build_initial_request("_0JZbsJdFIU")
     result= request.execute()
     parse_result(result, all_comments)
