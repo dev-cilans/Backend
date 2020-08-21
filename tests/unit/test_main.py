ed1d4b032fb95037a4db43918cb3af314ad0a2d5
YashSahsani <yashsahsani5@gmail.com>
2020-08-21 21:06:38 +0530

    Updated transcript endpoint

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -5,7 +5,7 @@ import sys
 try:
     sys.path.insert(1, 'src/')
 except:
-    print('Python path can't be set')
+    print("Python path can't be set")
 from main import app
 client = TestClient(app)
 def test_root():
@@ -25,7 +25,7 @@ def test_description():
     assert base64.b64encode(res.encode()) == base64.b64encode(response.content)
 def test_transcript():
     res = open('tests/unit/test_response/video_transcript.txt','r').read().strip()
-    response = client.get("/transcripts/2DG3pMcNNlw")
+    response = client.get("/transcripts/1ylleTbizgU")
     assert response.status_code == 200
     assert base64.b64encode(res.encode()) == base64.b64encode(response.content)
 def test_video_details():
