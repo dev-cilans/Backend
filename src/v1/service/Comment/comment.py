2c8a3d8620446edaf895025cfc597256e2def4c9
edwhuang <edwhuang@umich.edu>
2020-08-21 22:06:06 -0400

    bug fixes

--- src/v1/service/Comment/comment.py
+++ src/v1/service/Comment/comment.py
@@ -33,11 +33,11 @@ class Comment:
 		request = self.service.commentThreads().list(part="snippet", videoId=video_id)
 		return request
 
-	def parse_page(result, all_comments):
+	def parse_page(self, result, all_comments):
 		for item in result['items']:
 			# print(item['snippet']['topLevelComment']['snippet']['authorDisplayName'])
 			# print('---')
-			comment_text = item['snippet']['textDisplay']
+			comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
 			# print(comment_text)
 			# print(item['snippet']['topLevelComment']['snippet']['textOriginal'])
 			all_comments.append(comment_text)
@@ -45,8 +45,8 @@ class Comment:
 
 	def parse_result(self, result, all_comments, video_id):
 		while result.get('nextPageToken', False) and len(all_comments) <= 100:
-			parse_page(result, all_comments)
-			request = self.service.commendThreads().list(
+			self.parse_page(result, all_comments)
+			request = self.service.commentThreads().list(
 				part="snippet",
 				videoId=video_id,
 				pageToken=result.get('nextPageToken')
