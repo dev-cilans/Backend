e68b2bbd31501a7ac19c6ee89fed0547c81245d5
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2021-06-14 15:03:53 +0530

    lda

--- src/v1/service/Lda/lda.py
+++ src/v1/service/Lda/lda.py
@@ -1,10 +1,50 @@
-class Lda:
+import gensim
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+import numpy as np
+import nltk
+from youtube_transcript_api import YouTubeTranscriptApi
+
+class LDA:
     """ Description of Service """
+    from new_model import lemmatize_stemming,preprocess,dictionary,main
+    import joblib
+    lda_model = joblib.load(main())
+        
+    def retrieve_transcript(video_id):
+        output = YouTubeTranscriptApi.get_transcript(video_id)
+        segments = []
+        for e in output:
+            line = e['text']
+            line = line.replace('\n', '')
+            line = line.replace('>', '')
+            line = line.replace('--', '')
+            line = line.replace('♪', '')
+            segments.append(line)
 
-    def __init__(self, video_id: str):
-        """ Description of Method """
-        self.video_id = video_id
+        transcript = " ".join(segments)
+        return transcript
+    
+    def get(self):      
+        """ main method """
+        
+        try:
+            
+            bow_vector = dictionary.doc2bow(preprocess(retrieve_transcript(self.video_id)))
 
-    def get(self):
-        """ Description of Method """
-        return f"Lda get method result for {self.video_id}"
+            output = {}
+            for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
+                for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
+                    word_list = [w[0] for w in topic]
+                    output[index] = {
+                        "score" : score,
+                        "words" : word_list
+                        }
+                    print('Topic: {}\nScore:{} \nWords: {}\n'.format(index,score, [w[0] for w in topic]))  
+            from pprint import pprint
+            pprint(output,indent=3)
+    
+        
+        except:    
+            return {"Status":500,"error":"Some features of this video are disabled by youtube."}
+   
+    
