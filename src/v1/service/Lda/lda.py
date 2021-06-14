fca47135d46d1ffb5c234ad213bce7920fc07378
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2021-06-14 15:44:20 +0530

    lda

--- src/v1/service/Lda/lda.py
+++ src/v1/service/Lda/lda.py
@@ -5,12 +5,12 @@ import nltk
 from youtube_transcript_api import YouTubeTranscriptApi
 
 class LDA:
-    """ Description of Service """
     if __name__ == '__main__':
-        from new_model import lemmatize_stemming,preprocess,dictionary,main
+        from new_model import main,preprocess,dictionary,lemmatize_stemming
         import joblib
+        global lda_model
         lda_model = joblib.load(main())
-        
+            
         def retrieve_transcript(video_id):
             output = YouTubeTranscriptApi.get_transcript(video_id)
             segments = []
@@ -24,25 +24,25 @@ class LDA:
 
             transcript = " ".join(segments)
             return transcript
-    
-        def get(self):      
-        """ main method """
         
-            try:
+        def lda(transcript):
+            from new_model import preprocess,dictionary,lemmatize_stemming
+            bow_vector = dictionary.doc2bow(preprocess(transcript))
+            output = {}
+            for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
+                for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
+                    word_list = [w[0] for w in topic]
+                    output[index] = {
+                        "score" : score,
+                        "words" : word_list
+                        }
+                    print('Topic: {}\nScore:{} \nWords: {}\n'.format(index,score, [w[0] for w in topic]))  
+                    from pprint import pprint
+                    pprint(output,indent=3)
+        
+        def get(self):
+            video_transcript = self.retrieve_transcript(self.video_id)
+            lda = self.lda(video_transcript)
+            return lda
             
-                bow_vector = dictionary.doc2bow(preprocess(retrieve_transcript(self.video_id)))
-                output = {}
-                for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
-                    for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
-                        word_list = [w[0] for w in topic]
-                        output[index] = {
-                            "score" : score,
-                            "words" : word_list
-                            }
-                        print('Topic: {}\nScore:{} \nWords: {}\n'.format(index,score, [w[0] for w in topic]))  
-                from pprint import pprint
-                pprint(output,indent=3)
-            
-            except:    
-                return {"Status":500,"error":"Some features of this video are disabled by youtube."}
-   
+
