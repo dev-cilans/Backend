ce59ac1ee8574826a42505a7408f41d84a74f8b5
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2021-06-22 12:01:24 +0530

    sentiment

--- src/v1/service/Sentiment/sentiment.py
+++ src/v1/service/Sentiment/sentiment.py
@@ -6,6 +6,7 @@ import googleapiclient.discovery
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import numpy as np
 import nltk
+from pprint import pprint
 from youtube_transcript_api import YouTubeTranscriptApi
 analyser = SentimentIntensityAnalyzer()
 
@@ -41,63 +42,65 @@ class Sentiment:
 
     #Function takes in JSON response from Youtube V3 api and extracts comment text,likes and replies 
     def json_parser(self,json_response):
-                comments = []
-                likes = []
-                replies = []
-                for item in json_response.get('items'):
-                    comments.append(item.get('snippet').get('topLevelComment').get('snippet')['textOriginal'])
-                    likes.append(item.get('snippet').get('topLevelComment').get('snippet')['likeCount'])    
-                    replies.append(item.get('snippet')['totalReplyCount'])
-                assert len(comments) == len(likes) == len(replies),'Length not matching'
-                df = pd.DataFrame()
-                df['Comments'] = comments
-                df['Likes'] = likes
-                df['Replies'] = replies
-                return df
+        comments = []
+        likes = []
+        replies = []
+        for item in json_response.get('items'):
+            comments.append(item.get('snippet').get('topLevelComment').get('snippet')['textOriginal'])
+            likes.append(item.get('snippet').get('topLevelComment').get('snippet')['likeCount'])    
+            replies.append(item.get('snippet')['totalReplyCount'])
+        assert len(comments) == len(likes) == len(replies),'Length not matching'
+        df = pd.DataFrame()
+        df['Comments'] = comments
+        df['Likes'] = likes
+        df['Replies'] = replies
+        return df
+    
     #Function to scrape comments, their number likes and number replies using YouTube V3 API - NEEDS DEVELOPER_KEY
     def comment_scraper(self,video_id,max_comments):
                 # Disable OAuthlib's HTTPS verification when running locally.
                 # *DO NOT* leave this option enabled in production.
-                os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-                api_service_name="youtube"
-                api_version="v3"
-                DEVELOPER_KEY="AIzaSyDEyoZhTj3wh0B3r3evEmIxI-4g2Aa9-dE"
-                youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+        api_service_name="youtube"
+        api_version="v3"
+        DEVELOPER_KEY="AIzaSyDEyoZhTj3wh0B3r3evEmIxI-4g2Aa9-dE"
+        youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
                 
-                request = youtube.commentThreads().list(part="snippet,replies",maxResults=max_comments,order="relevance",videoId=video_id)
-                response=request.execute()
-                comment_df=self.json_parser(response)
-                comment_df['Sentiment'] = comment_df['Comments'].apply(lambda x : self.sentiment_scores(x))
-                return comment_df
+        request = youtube.commentThreads().list(part="snippet,replies",maxResults=max_comments,order="relevance",videoId=video_id)
+        response=request.execute()
+        comment_df=self.json_parser(response)
+        comment_df['Sentiment'] = comment_df['Comments'].apply(lambda x : self.sentiment_scores(x))
+        return comment_df
                                                       	
     def transcript_scrapper(self,video_id):
-                transcript=self.retrieve_transcript(video_id=self.video_id)
-                df1 = pd.DataFrame()
-                df1['Transcript'] =transcript
-                df1['Sentiment'] = df1['Transcript'].apply(lambda x : self.sentiment_scores(x))
-                return df1
+        transcript=self.retrieve_transcript(video_id=self.video_id)
+        transcript_df = pd.DataFrame()
+        transcript_df['Transcript'] = transcript
+        transcript_df['Sentiment'] = transcript_df['Transcript'].apply(lambda x: sentiment_scores(x))
+        return transcript_df
 
     def get(self):
         
-        df1 = self.transcript_scrapper(video_id= self.video_id)
-        neutral_trans = df1[df1['Sentiment'] == 'Neutral']['Transcript']
-        negative_trans = df1[df1['Sentiment'] == 'Negative']['Transcript']
-        positive_trans = df1[df1['Sentiment'] == 'Positive']['Transcript']
+        transcript_df = self.transcript_scrapper(video_id= self.video_id)
+        neutral_trans = transcript_df[transcript_df['Sentiment'] == 'Neutral']['Transcript']
+        negative_trans = transcript_df[transcript_df['Sentiment'] == 'Negative']['Transcript']
+        positive_trans = transcript_df[transcript_df['Sentiment']== 'Positive']['Transcript']
 
-        b=len(neutral_trans)+len(negative_trans)+len(positive_trans)
-        final_json1 = dict({"positive" : round(len(positive_trans)/b,3), "negative" : round(len(negative_trans)/b,3), "neutral" : round(len(neutral_trans)/b,3) })
-        print("{\n\tTranscripts:")
-        print(json.dumps(final_json1,indent=4),end="")
-        print(",")        
-        
+        trans_len = len(neutral_trans)+len(negative_trans)+len(positive_trans)
+        final_json_trans = dict({"positive": round(len(positive_trans)/trans_len, 3), "negative": round(len(negative_trans)/trans_len, 3), "neutral": round(len(neutral_trans)/trans_len, 3)})
+
         comment_df = self.comment_scraper(video_id= self.video_id,max_comments=self.max_num)
         neutral_comms = comment_df[comment_df['Sentiment'] == 'Neutral']['Comments']
         negative_comms = comment_df[comment_df['Sentiment'] == 'Negative']['Comments']
         positive_comments = comment_df[comment_df['Sentiment'] == 'Positive']['Comments']
         
-        a=len(neutral_comms)+len(negative_comms)+len(positive_comments)
-        final_json = dict({"positive" : round(len(positive_comments)/a,3), "negative" : round(len(negative_comms)/a,3), "neutral" : round(len(neutral_comms)/a,3) })
-        print("\tComments:")
-        print(json.dumps(final_json,indent=4)) #The final json file which contains comments categorized into json/dict with positive,negative and neutral keys
-        print("\n}")   
+        comms_len = len(neutral_comms)+len(negative_comms)+len(positive_comments)
+        final_json_comms = dict({"positive": round(len(positive_comments)/comms_len, 3), "negative": round(len(negative_comms)/comms_len, 3), "neutral": round(len(neutral_comms)/comms_len, 3)})
+        output = {
+        "Transcript": {},
+        "Comments": {}
+        }
+        output["Transcript"] = final_json_trans
+        output["Comments"] = final_json_comms
+        pprint(output, indent=1, width=1, compact=False) 
         
