e7b7b0b5d1c132d0291e4357afe3b8a090ee9800
kairavkkp <kairavpithadia13@gmail.com>
2021-06-22 13:58:04 +0530

    LDA API Working

--- src/v1/service/Lda/lda.py
+++ src/v1/service/Lda/lda.py
@@ -1,51 +1,57 @@
-import argparse
-import gensim
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
-import numpy as np
-import nltk
 from youtube_transcript_api import YouTubeTranscriptApi
-from model_updated import main as model
-from model_updated import get_dictionary_processed_docs, preprocess
+from .model import main as model
+from .model import get_dictionary_processed_docs, preprocess
 import joblib
-from pprint import pprint
 import warnings
 warnings.filterwarnings("ignore")
 
-class LDA:
-        
-        def __init__(self,video_id: str):
-            self.video_id = video_id
-        
-        def retrieve_transcript(self):
-            output = YouTubeTranscriptApi.get_transcript(self.video_id)
-            segments = []
-            for e in output:
-                line = e['text']
-                line = line.replace('\n', '')
-                line = line.replace('>', '')
-                line = line.replace('--', '')
-                line = line.replace('♪', '')
-                segments.append(line)
+class Lda:
+    """
+    Service for LDA Topic Modelling
+    """
+    def __init__(self, video_id: str):
+        """
+        Initializing Class
+        """
+        self.video_id = video_id
 
-            transcript = " ".join(segments)
-            return transcript
-        
-        def lda(self,transcript):
-            lda_model = joblib.load(model())
-            dictionary, _ = get_dictionary_processed_docs()
-            bow_vector = dictionary.doc2bow(preprocess(transcript))
+    def retrieve_transcript(self):
+        """
+        Helper to fetch the Transcript for the video.
+        """
+        output = YouTubeTranscriptApi.get_transcript(self.video_id)
+        segments = []
+        for e in output:
+            line = e['text']
+            line = line.replace('\n', '')
+            line = line.replace('>', '')
+            line = line.replace('--', '')
+            line = line.replace('♪', '')
+            segments.append(line)
 
-            output = {}
-            for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
-                for index, topic in lda_model.show_topics(formatted=False, num_words=10):
-                    word_list = [w[0] for w in topic]
-                    output[index] = {
-                        "score" : score,
-                        "words" : word_list
-                        }        
-            return output
-        
-        def get(self):
-            video_transcript = self.retrieve_transcript(video_id=self.video_id)
-            lda = self.lda(video_transcript)
-            return lda
+        transcript = " ".join(segments)
+        return transcript
+
+    def lda(self, transcript):
+        """
+        Helper to fetch the topics from the transcript.
+        """
+        lda_model = joblib.load(model())
+        dictionary, _ = get_dictionary_processed_docs()
+        bow_vector = dictionary.doc2bow(preprocess(transcript))
+
+        output = {}
+        for index_outer, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
+            for index_inner, topic in lda_model.show_topics(formatted=False, num_words=10):
+                word_list = [w[0] for w in topic]
+                output[str(index_inner)] = {
+                    "score": float(score),
+                    "words": word_list
+                }
+        return output
+
+    def get_topics(self):
+        video_transcript = self.retrieve_transcript()
+        lda = self.lda(video_transcript)
+        return lda
