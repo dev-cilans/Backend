a32aea3674491d46d4b72dff2d9f11e2f65cf957
Jhanvi <68721052+Jhanvi2001@users.noreply.github.com>
2021-06-22 12:34:08 +0530

    lda

--- src/v1/service/Lda/lda.py
+++ src/v1/service/Lda/lda.py
@@ -1,16 +1,18 @@
+import argparse
 import gensim
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 import numpy as np
 import nltk
 from youtube_transcript_api import YouTubeTranscriptApi
+from model_updated import main as model
+from model_updated import get_dictionary_processed_docs, preprocess
+import joblib
+from pprint import pprint
+import warnings
+warnings.filterwarnings("ignore")
 
 class LDA:
         
-        from new_model import main,preprocess,dictionary,lemmatize_stemming
-        import joblib
-        global lda_model
-        lda_model = joblib.load(main())
-         
         def __init__(self,video_id: str):
             self.video_id = video_id
         
@@ -28,24 +30,23 @@ class LDA:
             transcript = " ".join(segments)
             return transcript
         
-        def lda(self,transcript):
-            from new_model import preprocess,dictionary,lemmatize_stemming
-            bow_vector = dictionary.doc2bow(preprocess(transcript))
-            output = {}
-            for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
-                for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
-                    word_list = [w[0] for w in topic]
-                    output[index] = {
-                        "score" : score,
-                        "words" : word_list
-                        }
-                    print('Topic: {}\nScore:{} \nWords: {}\n'.format(index,score, [w[0] for w in topic]))  
-                    from pprint import pprint
-                    pprint(output,indent=3)
+        if __name__ == '__main__':
+            def lda(self,transcript):
+                lda_model = joblib.load(model())
+                dictionary, _ = get_dictionary_processed_docs()
+                bow_vector = dictionary.doc2bow(preprocess(transcript))
+
+                output = {}
+                for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1 * tup[1]):
+                    for index, topic in lda_model.show_topics(formatted=False, num_words=10):
+                        word_list = [w[0] for w in topic]
+                        output[index] = {
+                            "score" : score,
+                            "words" : word_list
+                            }        
+                        pprint(output,indent=3)
         
         def get(self):
             video_transcript = self.retrieve_transcript(video_id=self.video_id)
             lda = self.lda(video_transcript)
             return lda
-            
-
